f1738420710e4e8dad51ba89e66e2593931806ea
Thibault-GX <thibault.grimoux@gmail.com>
2019-10-25 09:55:29 +0200

    Push initial

---
+++ javascript.js
@@ -0,0 +1,7 @@
+function controle(){
+let firstNumber = document.getElementById("firstNumber").value;
+let secondNumber = document.getElementById("secondNumber").value;
+let result = firstNumber / secondNumber;
+
+alert(`Le résultat de la division de ces deux nombres est ${result}.`);
+}
