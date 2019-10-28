198383706e8aec64d3ba6643a869b2905fba81a9
Thibault-GX <thibault.grimoux@gmail.com>
2019-10-28 11:58:27 +0100

    Légère correction du JS pour déterminer le déclenchement directement depuis le fichier de script.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,13 @@
-function controle(){
-let firstNumber = document.getElementById("firstNumber").value;
-let secondNumber = document.getElementById("secondNumber").value;
-let result = firstNumber / secondNumber;
-
-alert(`Le résultat de la division de ces deux nombres est ${result}.`);
+let submit = document.getElementById('submit');
+submit.onclick = function()
+{
+  let firstNumber = document.getElementById("firstNumber").value;
+  let secondNumber = document.getElementById("secondNumber").value;
+  if (isNaN(firstNumber) || isNaN(secondNumber))
+  {
+    alert('Nous arnaque pas, coco. Faut rentrer des nombres !');
+    return false;
+  }
+  result = firstNumber / secondNumber;
+  alert(`Le résultat de la division de ces deux nombres est ${result}.`);
 }
